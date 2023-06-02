877d40196c05882cb21d2a8750acc5de9a3af4a9
Moises Pirela <moisespirela96@gmail.com>
2023-06-02 10:01:37 -0300

    add calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public enum OperationType
+{
+    Exponent = 0,
+    Addition,
+    Subtraction,
+    Division,
+    Multiplication,
+    Modulo
+}
+
+public struct Operation
+{
+    public OperationType OperationType;
+    public double[] Numbers;
+
+    public double Calculate()
+    {
+        switch (OperationType)
+        {
+            case OperationType.Exponent:
+                return Math.Pow(Numbers[0], Numbers[1]);
+            case OperationType.Addition:
+                return Numbers[0] + Numbers[1];
+            case OperationType.Subtraction:
+                return Numbers[0] - Numbers[1];
+            case OperationType.Division:
+                return Numbers[0] / Numbers[1];
+            case OperationType.Multiplication:
+                return Numbers[0] * Numbers[1];
+            case OperationType.Modulo:
+                return Numbers[0] % Numbers[1];
+        }
+        return 0;
+    }
+}
+
+public class Calculator
+{
+    private static readonly Dictionary<string, int> OperatorPrecedence = new Dictionary<string, int>()
+    {
+        {"+", 1},
+        {"-", 1},
+        {"*", 2},
+        {"/", 2},
+        {"%", 2},
+        {"^", 3}
+    };
+
+    private static double LastResult;
+    private static bool HasLastResult;
+
+    public static void Main(string[] args)
+    {
+        Console.Title = "Simple Calculator";
+
+        Console.WriteLine("Input an expression");
+
+        while (true)
+        {
+            if (HasLastResult)
+                Console.WriteLine($"Last result: {LastResult}");
+            
+            string userInput = Console.ReadLine();
+            
+            if (userInput == "clear")
+            {
+                HasLastResult = false;
+                LastResult = 0;
+            }
+            else if (IsValidInput(userInput))
+            {   
+                double result = EvaluateExpression(userInput);
+            
+                Console.WriteLine($"{userInput} = {result}");   
+            }
+        }
+    }
+
+    private static bool IsValidInput(string userInput)
+    {
+        int errors = Regex.Matches(userInput,@"[a-zA-Z]").Count;
+        
+        if (errors > 0)
+        {
+            Console.WriteLine("Please input a valid expression using digits and operators only (+ - * / ^ %)");
+        }
+
+        return errors == 0                      &&
+               !String.IsNullOrEmpty(userInput) && 
+               !string.IsNullOrWhiteSpace(userInput);
+
+    }
+
+    public static double EvaluateExpression(string expression)
+    {
+        string[] tokens = ParseExpression(expression);
+        Stack<double> numberStack = new Stack<double>();
+        Stack<string> operatorStack = new Stack<string>();
+
+        if (HasLastResult)
+            numberStack.Push(LastResult);
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            string token = tokens[i];
+            if (double.TryParse(token, out double number))
+            {
+                numberStack.Push(number);
+            }
+            else if (IsOperator(token))
+            {
+                if (token == "-" && (i == 0 || IsOperator(tokens[i - 1]) || tokens[i - 1] == "("))
+                {
+                    // Handle negative number
+                    if (double.TryParse(tokens[i + 1], out double negativeNumber))
+                    {
+                        numberStack.Push(-negativeNumber);
+                        i++; // Skip the next token since it has been processed as part of the negative number
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid expression: expected number after '-' operator");
+                    }
+                }
+                else
+                {
+                    while (operatorStack.Count > 0 &&
+                           IsOperator(operatorStack.Peek()) &&
+                           OperatorPrecedence[token] <= OperatorPrecedence[operatorStack.Peek()])
+                    {
+                        ApplyOperator(numberStack, operatorStack);
+                    }
+
+                    operatorStack.Push(token);
+                }
+            }
+        }
+
+        while (operatorStack.Count > 0)
+        {
+            ApplyOperator(numberStack, operatorStack);
+        }
+
+        return numberStack.Pop();
+    }
+
+    private static string[] ParseExpression(string expression)
+    {
+        string[] tokens = Regex.Split(expression, @"(\+|\-|\*|\/|\%|\^)");
+        List<string> sanitizedTokens = new List<string>();
+
+        foreach (string token in tokens)
+        {
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                sanitizedTokens.Add(token.Trim());
+            }
+        }
+
+        return sanitizedTokens.ToArray();
+    }
+
+    private static void ApplyOperator(Stack<double> numberStack, Stack<string> operatorStack)
+    {
+        string op = operatorStack.Pop();
+        double operand2 = numberStack.Pop();
+        double operand1 = numberStack.Pop();
+        double result = Calculate(operand1, operand2, op);
+        LastResult = result;
+        HasLastResult = true;
+        numberStack.Push(result);
+    }
+
+    private static double Calculate(double operand1, double operand2, string op)
+    {
+        OperationType operationType;
+
+        switch (op)
+        {
+            case "^":
+                operationType = OperationType.Exponent;
+                break;
+            case "+":
+                operationType = OperationType.Addition;
+                break;
+            case "-":
+                operationType = OperationType.Subtraction;
+                break;
+            case "*":
+                operationType = OperationType.Multiplication;
+                break;
+            case "/":
+                operationType = OperationType.Division;
+                break;
+            case "%":
+                operationType = OperationType.Modulo;
+                break;
+            default:
+                throw new InvalidOperationException($"Invalid operator: {op}");
+        }
+
+        Operation operation = new Operation()
+        {
+            OperationType = operationType,
+            Numbers = new double[] { operand1, operand2 }
+        };
+
+        return operation.Calculate();
+    }
+
+    private static bool IsOperator(string token)
+    {
+        return OperatorPrecedence.ContainsKey(token);
+    }
+}
---
+++ Calculator/CalculatorTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class CalculatorTests
+{
+    [TestCase("2 + 3", ExpectedResult = 5)]
+    [TestCase("10 - 5", ExpectedResult = 5)]
+    [TestCase("3 * 4", ExpectedResult = 12)]
+    [TestCase("20 / 5", ExpectedResult = 4)]
+    [TestCase("7 % 3", ExpectedResult = 1)]
+    [TestCase("2 ^ 3", ExpectedResult = 8)]
+    [TestCase("5 + 3 * 2", ExpectedResult = 11)]
+    [TestCase("(5 + 3) * 2", ExpectedResult = 16)]
+    [TestCase("-4 + 6", ExpectedResult = 2)]
+    [TestCase("3 - -2", ExpectedResult = 5)]
+    [TestCase("4 * -2", ExpectedResult = -8)]
+    [TestCase("10 / -5", ExpectedResult = -2)]
+    [TestCase("-7 % 3", ExpectedResult = -1)]
+    public double CalculateExpression_ValidInput_ReturnsExpectedResult(string expression)
+    {
+        return Calculator.EvaluateExpression(expression);
+    }
+
+    [TestCase("")]
+    [TestCase("   ")]
+    [TestCase("abc")]
+    [TestCase("2 +")]
+    [TestCase("5 3")]
+    [TestCase("(2 + 3")]
+    [TestCase("2 + 3)")]
+    [TestCase("2 3 +")]
+    [TestCase("2 / 0")]
+    public void CalculateExpression_InvalidInput_ThrowsException(string expression)
+    {
+        Assert.Throws<ArgumentException>(() => Calculator.EvaluateExpression(expression));
+    }
+}
